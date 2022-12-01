d6e672c28294dbc8aca1f9cc36e759ca44ba1712
FR_Jess <fr_jess@icloud.com>
2022-12-01 10:50:37 +0100

    [fix] correzione

--- index.php
+++ index.php
@@ -6,18 +6,32 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <!-- BOOTSTRAP -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.css' integrity='sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw==' crossorigin='anonymous'/>
   <title>Badwords</title>
 </head>
 
 <!-- Inizio Body -->
 
 <body>
+  <div class="container my-5">
 
-<form action="./landing-page.php" method="POST">
-    <input type="text" name="paragrafo" placeholder="scrivi qui quello che vuoi" /><br>
-    <input type="text" name="badword" placeholder="scrivi qui la parola da censurare" /><br>
-    <button type="submit">invia</button>
-  </form>
+    <form action="./landing-page.php" method="POST">
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Testo</label>
+        <textarea class="form-control" name="paragrafo" rows="3" ></textarea><br>
+        <label for="exampleFormControlTextArea1" class="form-label">Parola da censurare</label>
+        <input type="text" class="form-control" name="badword"></input><br>
+      </div>
+
+      <div class="mb-3">
+        <button class="btn btn-dark" type="submit">invia</button>
+      </div>
+
+    </form>
+
+  </div>
+
 
 </body>
 <!-- Fine Body -->
--- landing-page.php
+++ landing-page.php
@@ -1,7 +1,24 @@
 <?php
 
-$text = $_POST['paragrafo'];
-$badword = $_POST['badword'];
+
+if(isset($_POST['paragrafo'])){
+  $text = $_POST['paragrafo'];
+} else {
+  $text = '';
+};
+
+if(isset($_POST['badword'])){
+  $badword = $_POST ['badword'];
+} else {
+  $badword = '';
+};
+
+
+if(isset($_POST['badword'])){
+  $text_censored = str_replace($_POST['badword'], '***', $_POST['paragrafo']);
+} else{
+  $text_censored = '';
+}
 
 ?>
 
@@ -11,28 +28,29 @@ $badword = $_POST['badword'];
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- BOOTSTRAP -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.css' integrity='sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw==' crossorigin='anonymous'/>
   <title>Landing page</title>
 </head>
 
 <body>
+  <div class="container my-5">
 
-  <div>
-    Il tuo testo è il seguente: </br>
-    <?php echo $text ?>
-  </div></br>
-  <div>
-    La tua parola da censurare è: </br>
-    <?php echo $badword ?>
-  </div></br>
+    <h1> Badword</h1>
+    <p><?php echo $badword ?></p>
 
-  <div>
-    Il tuo testo è il seguente: </br>
-    <?php echo str_replace($badword, '***', $text); ?>
-  </div></br>
-  <div>
-    Il tuo testo lungo: 
-    <?php echo strlen($text)?> caratteri
-  </div></br>
+    <h2>Paragrafo originale</h2>
+    <p><?php echo $text ?></p>
+  
+    <h2>Paragrafo modificato</h2>
+    <p><?php echo $text_censored; ?></p>
+
+    <h2>Lunghezza</h2>
+    <p>Il tuo testo lungo <?php echo strlen($text)?> caratteri</p></br>
+
+  </div>
+
 
 </body>
 </html>
